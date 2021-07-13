ea29748b7339177b49092d491b7dc68d6dceaa3f
dango17 <danieloldham75@gmail.com>
2021-07-13 17:35:06 +0100

    MainMenu & TMP
    Options menu is still a WIP!

---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    { 
+        //String empty, main scene to go inside
+        SceneManager.LoadScene("");
+    } 
+
+    public void QuitGame()
+    { 
+        //Close Application
+        Application.Quit();  
+    }
+}
