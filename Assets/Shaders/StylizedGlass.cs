bd87f1d16702e68d149d6058d7bb62cb8f33efba
millwills1010 <74371042+millwills1010@users.noreply.github.com>
2021-07-13 12:42:52 +0100

    Glass Shader and Prefabs
    Finished Glass Shader and Added in fruit stall and lemonade stand prefabs

---
+++ Assets/Shaders/StylizedGlass.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionalLightShader : MonoBehaviour
+{
+
+    void Update()
+    {
+        Shader.SetGlobalVector("_LightDirectionVec", -transform.forward);
+
+    }
+}
