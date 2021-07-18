0a403cc9fd8644bac89fb3e121baae06883c687d
liambansal <55756009+liambansal@users.noreply.github.com>
2021-07-18 18:39:24 +0100

    Fixed Boxes Not Being Picked Up With LMB

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -2,99 +2,90 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PickUp : CrowdMaster
-{
-   
-    public Transform player, container;
+public class PickUp : CrowdMaster {
+	public float pickupRange;
 
-    public float pickupRange;
+	public bool holding;
+	public bool canPickup;
 
-    public bool holding;
-    public bool canPickup;
+	private Transform player = null;
+	/// <summary>
+	/// Position to place picked up objects.
+	/// </summary>
+	private Transform holdPosition = null;
 
-    private void Start()
-    {
-        player = GameObject.FindGameObjectWithTag("Player").transform;
-        block = FindObjectsOfType<Block>();
+	private void Start() {
+		player = GameObject.FindGameObjectWithTag("Player").transform;
+		holdPosition = GameObject.FindGameObjectWithTag("HoldPosition").transform;
+		block = FindObjectsOfType<Block>();
 
-        holding = false;
-        canPickup = false;
-    }
+		holding = false;
+		canPickup = false;
+	}
 
-    private void Update()
-    {
-        float distanceToBlock = float.MaxValue;
-        Block closestBlock = block[0];
-        foreach (Block bl in block)
-        {
-            float distBl = Vector3.Distance(player.transform.position, bl.transform.position);
-            if (distBl < distanceToBlock)
-            {
-                closestBlock = bl;
-                distanceToBlock = distBl;
-            }
-        }
+	private void Update() {
+		float distanceToBlock = float.MaxValue;
+		Block closestBlock = block[0];
+		foreach (Block bl in block) {
+			float distBl = Vector3.Distance(player.transform.position, bl.transform.position);
+			if (distBl < distanceToBlock) {
+				closestBlock = bl;
+				distanceToBlock = distBl;
+			}
+		}
 
-        if(!holding && distanceToBlock < pickupRange)
-        {
-            canPickup = true;
-        }
+		if (!holding && distanceToBlock < pickupRange) {
+			canPickup = true;
+		}
 
-        if(distanceToBlock > pickupRange)
-        {
-            canPickup = false;
-        }
-    }
+		if (distanceToBlock > pickupRange) {
+			canPickup = false;
+		}
+	}
 
-    public void PickUpObject()
-    {
-        holding = true;
+	public void PickUpObject() {
+		holding = true;
 
-        float distanceToBlock = float.MaxValue;
-        Block closestBlock = block[0];
-        foreach (Block bl in block)
-        {
-            float distBl = Vector3.Distance(player.transform.position, bl.transform.position);
-            if (distBl < distanceToBlock)
-            {
-                closestBlock = bl;
-                distanceToBlock = distBl;
-            }
-        }
+		float distanceToBlock = float.MaxValue;
+		Block closestBlock = block[0];
+		foreach (Block bl in block) {
+			float distBl = Vector3.Distance(player.transform.position, bl.transform.position);
+			if (distBl < distanceToBlock) {
+				closestBlock = bl;
+				distanceToBlock = distBl;
+			}
+		}
 
-        closestBlock.transform.SetParent(container);
-        closestBlock.transform.position = container.position;
+		closestBlock.transform.SetParent(holdPosition);
+		closestBlock.transform.position = holdPosition.position;
 
-        closestBlock.GetComponent<BoxCollider>().enabled = false;
-        closestBlock.GetComponent<Rigidbody>().useGravity = false;
-        closestBlock.GetComponent<Rigidbody>().freezeRotation = true;
-        closestBlock.GetComponent<Rigidbody>().isKinematic = true;
+		closestBlock.GetComponent<BoxCollider>().enabled = false;
+		closestBlock.GetComponent<Rigidbody>().useGravity = false;
+		closestBlock.GetComponent<Rigidbody>().freezeRotation = true;
+		closestBlock.GetComponent<Rigidbody>().isKinematic = true;
 
 
-    }
+	}
 
-    public void DropObject()
-    {
+	public void DropObject() {
 
-        float distanceToBlock = float.MaxValue;
-        Block closestBlock = block[0];
-        foreach (Block bl in block)
-        {
-            float distBl = Vector3.Distance(player.transform.position, bl.transform.position);
-            if (distBl < distanceToBlock)
-            {
-                closestBlock = bl;
-                distanceToBlock = distBl;
-            }
-        }
+		float distanceToBlock = float.MaxValue;
+		Block closestBlock = block[0];
+		foreach (Block bl in block) {
+			float distBl = Vector3.Distance(player.transform.position, bl.transform.position);
+			if (distBl < distanceToBlock) {
+				closestBlock = bl;
+				distanceToBlock = distBl;
+			}
+		}
 
-        holding = false;
+		holding = false;
 
-        closestBlock.transform.SetParent(null);
-        closestBlock.GetComponent<Rigidbody>().useGravity = true;
-        closestBlock.GetComponent<BoxCollider>().enabled = true;
-        closestBlock.GetComponent<Rigidbody>().freezeRotation = false;
-        closestBlock.GetComponent<Rigidbody>().isKinematic = false;
+		closestBlock.transform.SetParent(null);
+		closestBlock.GetComponent<Rigidbody>().useGravity = true;
+		closestBlock.GetComponent<BoxCollider>().enabled = true;
+		closestBlock.GetComponent<Rigidbody>().freezeRotation = false;
+		closestBlock.GetComponent<Rigidbody>().isKinematic = false;
 
-    }
+	}
 }
