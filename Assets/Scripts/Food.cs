12cae139dd56b29c8b26db3ba2b3d3018fd1f739
liambansal <55756009+liambansal@users.noreply.github.com>
2021-07-18 17:19:52 +0100

    Food SFX, Particle Effect and Fix for Destroying
    Sfx and particle effect now play when eating food.
    Fixed food not being destroyed after last use.

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class Food : Interactable {
@@ -9,20 +10,48 @@ public class Food : Interactable {
 	[SerializeField]
 	private float energy = 0.2f;
 
+	private ParticleSystem particleEffectSystem = null;
+	private AudioSource audioSource = null;
+
 	public override void Use() {
 		if (timesUsed < usesAvailable) {
 			isUsed = true;
 			GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerAbility>().ChangePowerLevel(energy);
+			particleEffectSystem.Play();
+			audioSource.Play();
 			++timesUsed;
 		}
 		
 		if (timesUsed >= usesAvailable) {
-			Destroy(gameObject);
-			Deselect();
+			StartCoroutine(Destroy());
 		}
+	}
+
+	private void Awake() {
+		particleEffectSystem = GetComponent<ParticleSystem>();
+		audioSource = GetComponent<AudioSource>();
 	}
 
 	private void Start() {
 		interactPrompt = GameObject.FindGameObjectWithTag("InteractPrompt").GetComponent<Text>();
+	}
+
+	private IEnumerator Destroy() {
+		// Wait until method returns true;
+		yield return new WaitUntil(ReadyToDestroy);
+		Deselect();
+		Destroy(gameObject);
+	}
+
+	/// <summary>
+	/// Returns if the script has completed all it's tasks e.g. playing audio.
+	/// </summary>
+	/// <returns> True if game object is okay to destroy. </returns>
+	private bool ReadyToDestroy() {
+		if (!audioSource.isPlaying && !particleEffectSystem.isPlaying) {
+			return true;
+		}
+
+		return false;
 	}
 }
