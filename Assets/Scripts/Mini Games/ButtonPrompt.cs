189597f080782dcb9638f9329600acd0ff21e74b
liambansal <55756009+liambansal@users.noreply.github.com>
2021-07-21 19:58:52 +0100

    Progress Bar For Button Presses/Hold
    Added a UI slider to the button prefab to show its progress of being used.

--- Assets/Scripts/Mini Games/ButtonPrompt.cs
+++ Assets/Scripts/Mini Games/ButtonPrompt.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class ButtonPrompt : MonoBehaviour {
 	public enum ButtonTypes {
@@ -12,6 +12,7 @@ public class ButtonPrompt : MonoBehaviour {
 
 	public int TimeToActivate { get { return TimeToActivate; } set { timeToActivate = value; } }
 	public bool DestroyOnUse { get { return destroyOnUse; } set { destroyOnUse = value; } }
+	public bool ShowProgressBar { get { return showProgressBar; } set { showProgressBar = value; } }
 	public ButtonTypes ButtonType { get { return buttonType; } set { buttonType = value; } }
 	public InputAction InputButton { get { return inputButton; } private set { } }
 
@@ -23,14 +24,20 @@ public class ButtonPrompt : MonoBehaviour {
 
 	private bool pressed = false;
 	private bool held = false;
+	[Tooltip("Is the button destroyed after being used.")]
 	[SerializeField]
 	private bool destroyOnUse = false;
+	[Tooltip("Show/hide a slider showing the progress of the button being used.")]
+	[SerializeField]
+	private bool showProgressBar = false;
 
 	private ButtonTypes buttonType = ButtonTypes.Press;
 
 	private MethodToExecute methodToExecute = null;
+	[Tooltip("The type of input to trigger the button.")]
 	[SerializeField]
 	private InputAction inputButton = null;
+	private Slider progressBar = null;
 
 	public delegate void MethodToExecute(ButtonPrompt buttonPrompt);
 
@@ -44,16 +51,37 @@ public class ButtonPrompt : MonoBehaviour {
 		}
 	}
 
-	public void LinkButton(MethodToExecute methodToLink) {
+	/// <summary>
+	/// Sets the method to call when the button is triggered.
+	/// </summary>
+	/// <param name="methodToLink"> Method to call when the button is triggered. </param>
+	public void LinkMethodToButton(MethodToExecute methodToLink) {
 		methodToExecute = methodToLink;
 	}
 
+	private void Awake() {
+		progressBar = GetComponent<Slider>();
+
+		if (!progressBar) {
+			progressBar = GetComponentInChildren<Slider>();
+		}
+	}
+
 	private void Start() {
+		progressBar.minValue = timesActivated;
+		progressBar.maxValue = timeToActivate;
+		progressBar.value = timesActivated;
 		inputButton.Enable();
 	}
 
 	// Update is called once per frame
 	private void Update() {
+		if (showProgressBar) {
+			progressBar.gameObject.SetActive(true);
+		} else {
+			progressBar.gameObject.SetActive(false);
+		}
+
 		inputButton.performed += i => held = true;
 		inputButton.started += i => pressed = true;
 		inputButton.canceled += i => held = false;
@@ -62,6 +90,7 @@ public class ButtonPrompt : MonoBehaviour {
 		if (buttonType == ButtonTypes.Hold) {
 			if (held) {
 				duration += Time.deltaTime;
+				progressBar.value = duration;
 
 				if (duration >= timeToActivate) {
 					Use();
@@ -71,6 +100,7 @@ public class ButtonPrompt : MonoBehaviour {
 			if (pressed) {
 				pressed = false;
 				++timesActivated;
+				progressBar.value = timesActivated;
 
 				if (timesActivated >= timeToActivate) {
 					Use();
