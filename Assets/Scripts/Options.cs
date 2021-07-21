5a2706410b7624fbd240f07e4a81752ab099b147
GDLinford <55755896+GDLinford@users.noreply.github.com>
2021-07-21 20:10:19 +0100

    more options stuff

--- Assets/Scripts/Options.cs
+++ Assets/Scripts/Options.cs
@@ -2,16 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class Options : MonoBehaviour
 {
-
     public Toggle fullscreen;
+    public Toggle Mute;
+
+    private bool isMuted;
+
+    public List<ResItem> resolutions = new List<ResItem>();
+    private int selectedRes;
+
+    public TMP_Text resLabels;
 
     // Start is called before the first frame update
     void Start()
     {
+        isMuted = false;
+
         fullscreen.isOn = Screen.fullScreen;
+        bool foundRes = false;
+
+        for(int i = 0; i < resolutions.Count; i++)
+        {
+            if(Screen.width == resolutions[i].horizontal && Screen.height == resolutions[i].vertical)
+            {
+                foundRes = true;
+                selectedRes = i;
+
+                UpdateResLabel();
+            }
+        }
+
+        if (!foundRes)
+        {
+            ResItem newRes = new ResItem();
+            newRes.horizontal = Screen.width;
+            newRes.vertical = Screen.height;
+
+            resolutions.Add(newRes);
+
+            selectedRes = resolutions.Count - 1;
+
+            UpdateResLabel();
+        }
     }
 
     // Update is called once per frame
@@ -20,8 +55,48 @@ public class Options : MonoBehaviour
         
     }
 
+    public void UpdateResLabel()
+    {
+        resLabels.text = resolutions[selectedRes].horizontal.ToString() + " x " + resolutions[selectedRes].vertical.ToString();
+    }
+
+    public void ResLeft()
+    {
+        selectedRes--;
+        if(selectedRes < 0)
+        {
+            selectedRes = 0;
+        }
+        UpdateResLabel();
+    }
+
+    public void ResRight()
+    {
+        selectedRes++;
+        if(selectedRes > resolutions.Count - 1)
+        {
+            selectedRes = resolutions.Count - 1; 
+        }
+
+        UpdateResLabel();
+    }
+
     public void Apply()
     {
-        Screen.fullScreen = fullscreen.isOn;
+        //Screen.fullScreen = fullscreen.isOn;
+        Screen.SetResolution(resolutions[selectedRes].horizontal, resolutions[selectedRes].vertical, fullscreen.isOn);
     }
+
+    public void MutePress()
+    {
+        isMuted = !isMuted;
+
+        AudioListener.pause = isMuted;
+    }
+}
+
+[System.Serializable]
+public class ResItem
+{
+    public int horizontal, vertical;
 }
