fecacae6520dea618cb98800b0ea9f8a15ebbdf3
GDLinford <55755896+GDLinford@users.noreply.github.com>
2021-07-21 16:28:12 +0100

    fix to the dance end animation

--- Assets/Scripts/Mini Games/Boombox/Boombox.cs
+++ Assets/Scripts/Mini Games/Boombox/Boombox.cs
@@ -2,25 +2,54 @@
 using UnityEngine.UI;
 
 public class Boombox : MiniGame {
+
+	//this is a very quick and dirty solution to the boombox problem
+	public float timer = 4.25f;
+	bool dancing;
+
 	private void Start() {
 		interactPrompt = GameObject.FindGameObjectWithTag("InteractPrompt").GetComponent<Text>();
 		inputManager = GameObject.FindGameObjectWithTag("Player").GetComponent<InputManager>();
+		dancing = false;
 	}
 
-	public override void Use() {
+    private void Update()
+    {
+		if(dancing == true)
+        {
+			timer -= Time.deltaTime;
+		}
+		
+		if (timer <= 0)
+		{
+			GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>().DancingComplete();
+			dancing = false;
+			timer = 4.25f;
+		}
+	}
+
+    public override void Use() {
 		if (timesUsed >= usesAvailable) {
 			return;
 		}
 
 		inputManager.SwitchInputMode(InputManager.InputModes.MiniGame);
+		dancing = true;
 		IsMiniGameActive = true;
 		GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>().SetBool("IsDancing", true);
+
 		++timesUsed;
+
+
+
+		
 	}
 
-	public override void CompleteEvent() {
+	public override void CompleteEvent() 
+	{
 		inputManager.SwitchInputMode(InputManager.InputModes.Player);
 		IsMiniGameActive = false;
 		GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>().SetBool("IsDancing", false);
+
 	}
 }
